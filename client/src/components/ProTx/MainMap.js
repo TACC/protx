a74056b21221ce6f0e8b4d798c5efa392c0ee246
Nathan Franklin <nathan.franklin@gmail.com>
2021-01-08 20:42:40 -0600

    Add initial map

--- client/src/components/ProTx/MainMap.js
+++ client/src/components/ProTx/MainMap.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import './MainMap.css';
+import 'leaflet/dist/leaflet.css';
 
 function MainMap() {
-  return <div>HELLO WORLD</div>;
+  let mapContainer;
+
+  useEffect(() => {
+    const initialState = {
+      lat: 32.7767,
+      lng: -96.797,
+      zoom: 12,
+      minZoom: 5,
+      maxZoom: 17
+    };
+
+    // eslint-disable-next-line no-unused-vars
+    const map = L.map(mapContainer).setView(
+      [initialState.lat, initialState.lng],
+      initialState.zoom
+    );
+
+    const basemapOsm = L.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        attribution:
+          'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 0,
+        maxZoom: 17,
+        name: 'OSM - Default',
+        type: 'png'
+      }
+    );
+
+    basemapOsm.addTo(map);
+  }, [mapContainer]);
+
+  return <div className="map-container" ref={el => (mapContainer = el)} />;
 }
 
 export default MainMap;
