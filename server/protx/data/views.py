c8e7c583a882603c5fad017b75e17e10feca8451
Nathan Franklin <nathan.franklin@gmail.com>
2021-01-13 15:27:08 -0600

    Stop requiring login for map

--- server/protx/data/views.py
+++ server/protx/data/views.py
@@ -1,9 +1,7 @@
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
 @ensure_csrf_cookie
 def data_map(request):
     return render(request, 'portal/apps/workbench/index.html')
