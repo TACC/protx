e0a21de52a121527d955efbe2046d943d48a2ccb
Nathan Franklin <nathan.franklin@gmail.com>
2021-01-13 20:24:24 -0600

    Add placeholder drop downs

--- client/src/components/ProTx/MainMap.js
+++ client/src/components/ProTx/MainMap.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
+import { DropdownSelector } from '_common';
 import MapProviders from './MapProviders';
 import './MainMap.css';
+import './MainMap.module.scss';
 import 'leaflet/dist/leaflet.css';
 
 function MainMap() {
@@ -28,7 +30,53 @@ function MainMap() {
     L.control.layers(baseMaps, overlayMaps).addTo(map);
   }, [mapContainer]);
 
-  return <div className="map-container" ref={el => (mapContainer = el)} />;
+  return (
+    <div styleName="root">
+      <div styleName="control-bar-container">
+        <div styleName="control">
+          <span styleName="label">Select Area</span>
+          <DropdownSelector>
+            <optgroup label="Select Areas">
+              <option value="dfpsRegions">DFPS Regions</option>
+              <option value="counties">Counties</option>
+              <option value="metropolitanAreas">Metropolitan Areas</option>
+              <option value="cities">Cities</option>
+              <option value="zipCodes">Zip Codes</option>
+              <option value="schoolDistricts">School Districts</option>
+              <option value="censusTracts">Census Tracts</option>
+            </optgroup>
+          </DropdownSelector>
+        </div>
+        <div styleName="control">
+          <span styleName="label">Select Display</span>
+          <DropdownSelector>
+            <optgroup label="Select Display">
+              <option value="allCIs">All CIs</option>
+              <option value="selectCIs">Select CIs</option>
+              <option value="trendOverTime">Trend Over Time</option>
+              <option value="thresholds">Thresholds</option>
+              <option value="nbhdSocialTapestry">NbhdS ocialTapestry</option>
+              <option value="socialCorrelates">Social Correlates</option>
+              <option value="predictions">Predictions</option>
+              <option value="resources">Sesources</option>
+            </optgroup>
+          </DropdownSelector>
+        </div>
+        <div styleName="control">
+          <span styleName="label">Select TimeFrame</span>
+          <DropdownSelector disabled>
+            <optgroup label="Select Timeframe" />
+          </DropdownSelector>
+        </div>
+      </div>
+      <div
+        styleName="map"
+        className="map-container"
+        ref={el => (mapContainer = el)}
+      />
+      ;
+    </div>
+  );
 }
 
 export default MainMap;
