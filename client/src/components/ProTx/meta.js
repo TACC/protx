7d053d21749c224905df11dc2177444df861f951
Nathan Franklin <nathan.franklin@gmail.com>
2021-04-13 10:40:42 -0500

    Reorder features to EP follows corresponding E

--- client/src/components/ProTx/meta.js
+++ client/src/components/ProTx/meta.js
@@ -1,37 +1,31 @@
 export const OBSERVED_FEATURES = [
-  {field: 'EP_CROWD', 'Estimated percent crowding'},
-  {field: 'EP_LIMENG', 'Estimated percent population with limited English skills'},
   { field: 'E_AGE17', name: 'Estimated population age 17 or younger' },
   { field: 'E_AGE65', name: 'Estimated population age 65 or older' },
   { field: 'E_CROWD', name: 'Estimated crowding' },
+  { field: 'EP_CROWD', name: 'Estimated percent crowding' },
   { field: 'E_DISABL', name: 'Estimated disabled population' },
+  { field: 'EP_DISABL', name: 'Estimated percent disabled population'},
   { field: 'E_GROUPQ', name: 'Estimated population living in group quarters' },
   { field: 'E_HH', name: 'Estimated households' },
   { field: 'E_HU', name: 'Estimated housing units' },
-  {
-    field: 'E_LIMENG',
-    name: 'Estimated population with limited English skills'
-  },
+  { field: 'E_LIMENG', name: 'Estimated population with limited English skills'},
+  { field: 'EP_LIMENG', name: 'Estimated percent population with limited English skills' },
   { field: 'E_MOBILE', name: 'Estimated mobile homes' },
+  { field: 'EP_MOBILE', name: 'Estimated percent mobile homes'},
   { field: 'E_MUNIT', name: 'Estimated number of multi-unit structures' },
-  {
-    field: 'E_NOHSDP',
-    name: 'Estimated population with no high school diploma'
-  },
+  { field: 'E_NOHSDP', name: 'Estimated population with no high school diploma'},
+  { field: 'EP_NOHSDP', name:'Estimated percent population with no high school diploma'},
   { field: 'E_NOVEH', name: 'Estimated households with no vehicle' },
+  { field: 'EP_NOVEH', name: 'Estimated percent households with no vehicle'},
   { field: 'E_PCI', name: 'Estimated per capita income' },
   { field: 'E_POV', name: 'Estimated population below poverty threshold' },
+  { field: 'EP_POV', name:'Estimated percent population below poverty threshold'},
   { field: 'E_SNGPNT', name: 'Estimated single parent households' },
   { field: 'E_TOTPOP', name: 'Estimated total population' },
   { field: 'E_UNEMP', name: 'Estimated unemployed population' },
+  { field: 'EP_UNEMP', name: 'Estimated percent unemployed population'},
   { field: 'E_UNINSUR', name: 'Estimated uninsured population' },
-  { field: 'EP_DISABL', name:'Estimated percent disabled population'},
-  { field: 'EP_MOBILE', name:'Estimated percent mobile homes'},
-  { field: 'EP_NOHSDP', name:'Estimated percent population with no high school diploma'},
-  { field: 'EP_NOVEH', name:'Estimated percent households with no vehicle'},
-  { field: 'EP_POV', name:'stimated percent population below poverty threshold'},
-  { field: 'EP_UNEMP', name:'Estimated percent unemployed population'},
-  { field: 'EP_UNINSUR', name:'Estimated percent uninsured population'},
+  { field: 'EP_UNINSUR', name: 'Estimated percent uninsured population'},
   { field: 'E_MINRTY', name: 'Estimated minority population' }
 ];
 
