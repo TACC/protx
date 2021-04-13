2001c090c6838c93df700c38a0e7b04828a52889
Nathan Franklin <nathan.franklin@gmail.com>
2021-04-13 10:24:36 -0500

    Add EP_* values

--- client/src/components/ProTx/meta.js
+++ client/src/components/ProTx/meta.js
@@ -1,7 +1,9 @@
 export const OBSERVED_FEATURES = [
+  {field: 'EP_CROWD', 'Estimated percent crowding'},
+  {field: 'EP_LIMENG', 'Estimated percent population with limited English skills'},
   { field: 'E_AGE17', name: 'Estimated population age 17 or younger' },
   { field: 'E_AGE65', name: 'Estimated population age 65 or older' },
-  { field: 'E_CROWD', name: 'Estimated percent crowding' },
+  { field: 'E_CROWD', name: 'Estimated crowding' },
   { field: 'E_DISABL', name: 'Estimated disabled population' },
   { field: 'E_GROUPQ', name: 'Estimated population living in group quarters' },
   { field: 'E_HH', name: 'Estimated households' },
@@ -23,8 +25,16 @@ export const OBSERVED_FEATURES = [
   { field: 'E_TOTPOP', name: 'Estimated total population' },
   { field: 'E_UNEMP', name: 'Estimated unemployed population' },
   { field: 'E_UNINSUR', name: 'Estimated uninsured population' },
+  { field: 'EP_DISABL', name:'Estimated percent disabled population'},
+  { field: 'EP_MOBILE', name:'Estimated percent mobile homes'},
+  { field: 'EP_NOHSDP', name:'Estimated percent population with no high school diploma'},
+  { field: 'EP_NOVEH', name:'Estimated percent households with no vehicle'},
+  { field: 'EP_POV', name:'stimated percent population below poverty threshold'},
+  { field: 'EP_UNEMP', name:'Estimated percent unemployed population'},
+  { field: 'EP_UNINSUR', name:'Estimated percent uninsured population'},
   { field: 'E_MINRTY', name: 'Estimated minority population' }
 ];
+
 
 export const MALTREATMENT = [
   { field: 'ALL', name: 'All' },
