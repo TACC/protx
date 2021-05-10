d7440ca6de47965eecd2fa02004e5bc073c28aeb
John Gentle <taoteg@gmail.com>
2021-05-10 17:01:23 -0500

    Removed all but the selector bar backing colors. Added a basic react-plotly chart. Working on making chart responsive and stateful.

---
+++ client/src/components/ProTx/ChartHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChartHeader.css';
+
+function ChartHeader() {
+  return (
+    <div className="chart-header">
+      <h3>Chart Information</h3>
+      <p>Details about the chart being displayed.</p>
+      <hr />
+    </div>
+  );
+}
+
+export default ChartHeader;
--- client/src/components/ProTx/MainChart.js
+++ client/src/components/ProTx/MainChart.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import ChartHeader from './ChartHeader';
+import ChartDiagram from './ChartDiagram';
 import './MainChart.css';
-// import styles from './MainChart.css';
 
 function MainChart() {
   return (
     <div className="main-chart">
-      <h3>CHART GOES HERE</h3>
-      <p>Content pending</p>
-      <hr />
+      <ChartHeader className="chart-header" />
+      <ChartDiagram className="chart-diagram" />
     </div>
   );
 }
