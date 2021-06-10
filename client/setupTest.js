b7e6228ea0210591d217998fe8479ce32464fbb2
Nathan Franklin <nathan.franklin@gmail.com>
2021-06-10 11:27:45 -0500

    Fix unit tests related to plotly

---
+++ client/setupTest.js
@@ -0,0 +1,8 @@
+// following are related to plotly and jest
+// https://stackoverflow.com/questions/48828759/unit-test-raises-error-because-of-getcontext-is-not-implemented
+// https://github.com/plotly/react-plotly.js/issues/115
+window.URL.createObjectURL = function() {};
+
+HTMLCanvasElement.prototype.getContext = () => {
+  // return whatever getContext has to return
+};
