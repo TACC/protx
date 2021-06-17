54bd5cc5857cfe125f774f93b576b3a7e1fc9f4c
John Gentle <taoteg@gmail.com>
2021-06-17 13:39:36 -0500

    Cleaned up StackedBar example and styled to match data sets we have and example from Cooks team. Moving to new ConfigurableChart component to handle multiple cases.

--- client/src/components/ProTx/components/charts/StackedBarChart.js
+++ client/src/components/ProTx/components/charts/StackedBarChart.js
@@ -2,56 +2,125 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 import './StackedBarChart.css';
 // import {
-//   // getMetaData,
-//   // getMaltreatmentAggregatedValue,
+//   getMetaData,
+//   getMaltreatmentAggregatedValue,
 //   getObservedFeatureValue
 // } from '../util';
 
 function StackedBarChart() {
   // const ofv = getObservedFeatureValue();
 
-  const state = {
-    data: [
-      // {
-      //   x: [1, 2, 3, 4, 5, 6, 7],
-      //   y: [1, 6, 3, 11, 4, 9, 13],
-      //   type: 'scatter',
-      //   mode: 'lines+markers',
-      //   marker: { color: 'red' }
-      // },
-      // { type: 'bar', x: [1, 2, 3, 4, 5, 6, 7], y: [2, 6, 3, 7, 4, 9, 15] }
-      {
-        x: [20, 14, 23, 76, 129],
-        y: ['giraffes', 'orangutans', 'monkeys'],
-        name: 'SF Zoo',
-        type: 'bar',
-        orientation: 'h'
-      },
-      {
-        x: [12, 18, 29, 64, 173],
-        y: ['giraffes', 'orangutans', 'monkeys'],
-        name: 'LA Zoo',
-        type: 'bar',
-        orientation: 'h'
-      }
+  const chartConfig = {
+    doubleClickDelay: 1000,
+    responsive: true,
+    useResizeHandler: true,
+    displayModeBar: false,
+    modeBarButtonsToRemove: [
+      /* An array of interface buttons to remove, e.g. 'toImage'.
+      Docs: https://plotly.com/javascript/configuration-options/#remove-modebar-buttons
+      */
     ],
-    layout: {
-      width: '100%',
-      height: '100%',
-      barmode: 'stack',
-      title: 'Stacked Bar Chart - Horizontal'
-    },
-    config: {
-      displaylogo: false,
-      // showEditInChartStudio: true,
-      // plotlyServerURL: 'https://chart-studio.plotly.com',
-      // linkText: 'Edit Chart in Plotly Studio',
-      responsive: true,
-      doubleClickDelay: 1000,
-      useResizeHandler: true
-    }
+    displaylogo: false,
+    showEditInChartStudio: false
+    // enable these values of you set showEditInChartStudio to true.
+    // plotlyServerURL: 'https://chart-studio.plotly.com',
+    // linkText: 'Edit Chart in Plotly Studio'
   };
 
+  const chartLayout = {
+    width: '100%',
+    height: '100%',
+    barmode: 'stack',
+    title: 'Stacked Bar Chart - Horizontal'
+  };
+
+  const chartData = [
+    // Order objects in reverse of how you want them to populate the category key.
+    {
+      x: [50, 100, 75, 25, 100, 30, 70, 60, 20, 90],
+      y: [
+        '2010',
+        '2011',
+        '2012',
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019'
+      ],
+      name: 'Maltreatment Type D',
+      type: 'bar',
+      orientation: 'h'
+    },
+    {
+      x: [10, 90, 25, 45, 50, 60, 30, 20, 40, 80],
+      y: [
+        '2010',
+        '2011',
+        '2012',
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019'
+      ],
+      name: 'Maltreatment Type C',
+      type: 'bar',
+      orientation: 'h'
+    },
+    {
+      x: [70, 10, 45, 50, 10, 20, 45, 15, 80, 20],
+      y: [
+        '2010',
+        '2011',
+        '2012',
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019'
+      ],
+      name: 'Maltreatment Type B',
+      type: 'bar',
+      orientation: 'h'
+    },
+    {
+      x: [5, 10, 7, 2, 10, 3, 7, 6, 2, 9],
+      y: [
+        '2010',
+        '2011',
+        '2012',
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019'
+      ],
+      name: 'Maltreatment Type A',
+      type: 'bar',
+      orientation: 'h'
+    }
+  ];
+
+  const state = {
+    data: chartData,
+    layout: chartLayout,
+    config: chartConfig
+  };
+
+  // console.log(ofv);
+  // console.log(ofv);
+  // console.log(ofv);
+  // console.log(ofv);
+  // console.log(ofv);
   // console.log(ofv);
 
   return (
@@ -61,51 +130,45 @@ function StackedBarChart() {
           <div className="chart-info">
             <div className="chart-info-item">
               <div className="chart-info-title">Selected Year</div>
-              <div className="chart-info-value">2021</div>
+              <div className="chart-info-value">DEMO</div>
             </div>
             <div className="chart-info-item">
               <div className="chart-info-title">Element</div>
-              <div className="chart-info-value">Dallas County</div>
+              <div className="chart-info-value">Demo County</div>
             </div>
             <div className="chart-info-item">
               <div className="chart-info-title">County</div>
-              <div className="chart-info-value">Dallas</div>
+              <div className="chart-info-value">Demo</div>
             </div>
             <div className="chart-info-item">
               <div className="chart-info-title">Victims/1,000 Children</div>
-              <div className="chart-info-value">41.94</div>
+              <div className="chart-info-value">##.##</div>
             </div>
           </div>
         </div>
         <div className="chart-body">
-          <div className="chart-body-subtitle">Trend Past 5 Years</div>
+          {/* <div className="chart-body-subtitle">Trend Past 10 Years</div> */}
           <Plot data={state.data} layout={state.layout} config={state.config} />
         </div>
         <div className="chart-footer">
           <div className="chart-links">
-            <p>
+            <p className="chart-link-item">
+              Show
               <a href="#" target="">
-                Link
-              </a>{' '}
-              to supporting chart data.
+                social tapestry information
+              </a>
             </p>
-            <p>
+            <p className="chart-link-item">
+              Show
               <a href="#" target="">
-                Link
-              </a>{' '}
-              to supporting chart data.
+                best predictors for this area
+              </a>
             </p>
-            <p>
+            <p className="chart-link-item">
+              Show
               <a href="#" target="">
-                Link
-              </a>{' '}
-              to supporting chart data.
-            </p>
-            <p>
-              <a href="#" target="">
-                Link
-              </a>{' '}
-              to supporting chart data.
+                available resources in this area
+              </a>
             </p>
           </div>
         </div>
