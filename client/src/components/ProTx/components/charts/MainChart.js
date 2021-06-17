22dca75f211e347d25a3d1b5e3ce2a8dc0e521b6
John Gentle <taoteg@gmail.com>
2021-06-17 18:47:53 -0500

    Removed some unused cull for old charts. Updated a css selector for the chart layout on header info.

--- client/src/components/ProTx/components/charts/MainChart.js
+++ client/src/components/ProTx/components/charts/MainChart.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './MainChart.css';
-// import ScatterBarChart from './ScatterBarChart';
-// import StackedBarChart from './StackedBarChart';
 import ConfigurableChart from './ConfigurableChart';
 
 function MainChart() {
   return (
     <div className="main-chart">
-      {/* <StackedBarChart className="chart-diagram" /> */}
       <ConfigurableChart className="chart-diagram" />
     </div>
   );
