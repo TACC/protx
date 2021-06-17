2efe0444308b4ee47bc8229becf09e675d3b3108
John Gentle <taoteg@gmail.com>
2021-06-17 14:01:06 -0500

    Adding an external config to chart for better control and easier code comprehension.

--- client/src/components/ProTx/components/charts/StackedBarChart.js
+++ client/src/components/ProTx/components/charts/StackedBarChart.js
@@ -1,127 +1,24 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import './StackedBarChart.css';
+import { CHART_CONFIG, CHART_LAYOUT, CHART_DATA } from './chartConfig.01';
+
 // import {
 //   getMetaData,
 //   getMaltreatmentAggregatedValue,
 //   getObservedFeatureValue
 // } from '../util';
 
+// Replace test data with actual data from selection.
+// const ofv = getObservedFeatureValue();
+// Order objects in reverse of how you want them to populate the category key.
+
 function StackedBarChart() {
-  // const ofv = getObservedFeatureValue();
-
-  const chartConfig = {
-    doubleClickDelay: 1000,
-    responsive: true,
-    useResizeHandler: true,
-    displayModeBar: false,
-    modeBarButtonsToRemove: [
-      /* An array of interface buttons to remove, e.g. 'toImage'.
-      Docs: https://plotly.com/javascript/configuration-options/#remove-modebar-buttons
-      */
-    ],
-    displaylogo: false,
-    showEditInChartStudio: false
-    // enable these values of you set showEditInChartStudio to true.
-    // plotlyServerURL: 'https://chart-studio.plotly.com',
-    // linkText: 'Edit Chart in Plotly Studio'
-  };
-
-  const chartLayout = {
-    width: '100%',
-    height: '100%',
-    barmode: 'stack',
-    title: 'Stacked Bar Chart - Horizontal'
-  };
-
-  const chartData = [
-    // Order objects in reverse of how you want them to populate the category key.
-    {
-      x: [50, 100, 75, 25, 100, 30, 70, 60, 20, 90],
-      y: [
-        '2010',
-        '2011',
-        '2012',
-        '2013',
-        '2014',
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019'
-      ],
-      name: 'Maltreatment Type D',
-      type: 'bar',
-      orientation: 'h'
-    },
-    {
-      x: [10, 90, 25, 45, 50, 60, 30, 20, 40, 80],
-      y: [
-        '2010',
-        '2011',
-        '2012',
-        '2013',
-        '2014',
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019'
-      ],
-      name: 'Maltreatment Type C',
-      type: 'bar',
-      orientation: 'h'
-    },
-    {
-      x: [70, 10, 45, 50, 10, 20, 45, 15, 80, 20],
-      y: [
-        '2010',
-        '2011',
-        '2012',
-        '2013',
-        '2014',
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019'
-      ],
-      name: 'Maltreatment Type B',
-      type: 'bar',
-      orientation: 'h'
-    },
-    {
-      x: [5, 10, 7, 2, 10, 3, 7, 6, 2, 9],
-      y: [
-        '2010',
-        '2011',
-        '2012',
-        '2013',
-        '2014',
-        '2015',
-        '2016',
-        '2017',
-        '2018',
-        '2019'
-      ],
-      name: 'Maltreatment Type A',
-      type: 'bar',
-      orientation: 'h'
-    }
-  ];
-
   const state = {
-    data: chartData,
-    layout: chartLayout,
-    config: chartConfig
+    data: CHART_DATA,
+    layout: CHART_LAYOUT,
+    config: CHART_CONFIG
   };
-
-  // console.log(ofv);
-  // console.log(ofv);
-  // console.log(ofv);
-  // console.log(ofv);
-  // console.log(ofv);
-  // console.log(ofv);
 
   return (
     <div className="stacked-bar-chart">
