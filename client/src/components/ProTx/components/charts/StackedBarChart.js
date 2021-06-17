dcf502262ac15b9e9a0e6f629cdc0eed17594848
John Gentle <taoteg@gmail.com>
2021-06-17 11:59:10 -0500

    Established basic horizontal barchart for maltreatment. No live data integration yet.

--- client/src/components/ProTx/components/charts/StackedBarChart.js
+++ client/src/components/ProTx/components/charts/StackedBarChart.js
@@ -1,34 +1,58 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import './StackedBarChart.css';
+// import {
+//   // getMetaData,
+//   // getMaltreatmentAggregatedValue,
+//   getObservedFeatureValue
+// } from '../util';
 
 function StackedBarChart() {
+  // const ofv = getObservedFeatureValue();
+
   const state = {
     data: [
+      // {
+      //   x: [1, 2, 3, 4, 5, 6, 7],
+      //   y: [1, 6, 3, 11, 4, 9, 13],
+      //   type: 'scatter',
+      //   mode: 'lines+markers',
+      //   marker: { color: 'red' }
+      // },
+      // { type: 'bar', x: [1, 2, 3, 4, 5, 6, 7], y: [2, 6, 3, 7, 4, 9, 15] }
       {
-        x: [1, 2, 3, 4, 5, 6, 7],
-        y: [1, 6, 3, 11, 4, 9, 13],
-        type: 'scatter',
-        mode: 'lines+markers',
-        marker: { color: 'red' }
+        x: [20, 14, 23, 76, 129],
+        y: ['giraffes', 'orangutans', 'monkeys'],
+        name: 'SF Zoo',
+        type: 'bar',
+        orientation: 'h'
       },
-      { type: 'bar', x: [1, 2, 3, 4, 5, 6, 7], y: [2, 6, 3, 7, 4, 9, 15] }
+      {
+        x: [12, 18, 29, 64, 173],
+        y: ['giraffes', 'orangutans', 'monkeys'],
+        name: 'LA Zoo',
+        type: 'bar',
+        orientation: 'h'
+      }
     ],
     layout: {
       width: '100%',
       height: '100%',
-      title: 'Scatterplot w/Bar Chart Example'
+      barmode: 'stack',
+      title: 'Stacked Bar Chart - Horizontal'
     },
     config: {
       displaylogo: false,
-      showEditInChartStudio: true,
-      plotlyServerURL: 'https://chart-studio.plotly.com',
-      linkText: 'Edit Chart in Plotly Studio',
+      // showEditInChartStudio: true,
+      // plotlyServerURL: 'https://chart-studio.plotly.com',
+      // linkText: 'Edit Chart in Plotly Studio',
       responsive: true,
       doubleClickDelay: 1000,
       useResizeHandler: true
     }
   };
+
+  // console.log(ofv);
 
   return (
     <div className="stacked-bar-chart">
@@ -59,10 +83,30 @@ function StackedBarChart() {
         </div>
         <div className="chart-footer">
           <div className="chart-links">
-            <p><a href="#" target="">Link</a> to supporting chart data.</p>
-            <p><a href="#" target="">Link</a> to supporting chart data.</p>
-            <p><a href="#" target="">Link</a> to supporting chart data.</p>
-            <p><a href="#" target="">Link</a> to supporting chart data.</p>
+            <p>
+              <a href="#" target="">
+                Link
+              </a>{' '}
+              to supporting chart data.
+            </p>
+            <p>
+              <a href="#" target="">
+                Link
+              </a>{' '}
+              to supporting chart data.
+            </p>
+            <p>
+              <a href="#" target="">
+                Link
+              </a>{' '}
+              to supporting chart data.
+            </p>
+            <p>
+              <a href="#" target="">
+                Link
+              </a>{' '}
+              to supporting chart data.
+            </p>
           </div>
         </div>
       </div>
@@ -71,29 +115,3 @@ function StackedBarChart() {
 }
 
 export default StackedBarChart;
-
-
-
-/*
-// Plotly Stacked Bar.
-
-var trace1 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [20, 14, 23],
-  name: 'SF Zoo',
-  type: 'bar'
-};
-
-var trace2 = {
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [12, 18, 29],
-  name: 'LA Zoo',
-  type: 'bar'
-};
-
-var data = [trace1, trace2];
-
-var layout = { barmode: 'stack' };
-
-Plotly.newPlot('myDiv', data, layout);
-*/
