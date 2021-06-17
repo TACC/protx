a6bd99144e688919a7b876e09e7a081903ff347d
John Gentle <taoteg@gmail.com>
2021-06-17 17:52:16 -0500

    Established configurable chart component and created multiple configurations to drive the UI. Custom color scales are wired in for bars.

--- client/src/components/ProTx/components/charts/MainChart.js
+++ client/src/components/ProTx/components/charts/MainChart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './MainChart.css';
 // import ScatterBarChart from './ScatterBarChart';
-import StackedBarChart from './StackedBarChart';
+// import StackedBarChart from './StackedBarChart';
+import ConfigurableChart from './ConfigurableChart';
 
 function MainChart() {
   return (
     <div className="main-chart">
-      <StackedBarChart className="chart-diagram" />
+      {/* <StackedBarChart className="chart-diagram" /> */}
+      <ConfigurableChart className="chart-diagram" />
     </div>
   );
 }
