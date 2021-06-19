9727492460a46fc7f1793b31e936287b8248d9ae
John Gentle <taoteg@gmail.com>
2021-06-18 19:33:46 -0500

    Refactcored out one assignment line that was unnecessary.

--- client/src/components/ProTx/components/charts/configs/chart.01.js
+++ client/src/components/ProTx/components/charts/configs/chart.01.js
@@ -1,15 +1,13 @@
 import { INFO_DEMO } from './chart.info';
-import { CONFIG_01 } from './chart.configs';
+import { CONFIG_BASE } from './chart.configs';
 import { LAYOUT_01 } from './chart.layouts';
 import { TRACE_HORZ_SINGLE_ALL } from './trace.data';
 
-const DATA_ARRAY = TRACE_HORZ_SINGLE_ALL;
-
 const CONFIGURATION = {
   CHART_INFO: INFO_DEMO,
-  CHART_CONFIG: CONFIG_01,
+  CHART_CONFIG: CONFIG_BASE,
   CHART_LAYOUT: LAYOUT_01,
-  CHART_DATA: DATA_ARRAY
+  CHART_DATA: TRACE_HORZ_SINGLE_ALL
 };
 
 export default CONFIGURATION;
