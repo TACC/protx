be257be5b1565e1f186ec7c95266d92c6a8fe8db
John Gentle <taoteg@gmail.com>
2021-07-05 23:59:22 -0500

    Refactored the plots into unique components. Refactored the debug info out of the plots. Continuing to refactor ConfigurableChart into the MaltreatmentTypesPlot component and PredictiveFeaturesPlot components.

---
+++ client/src/components/ProTx/components/charts/DebugPlot.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ConfigurableChart.css';
+import { OBSERVED_FEATURES, MALTREATMENT } from '../meta';
+import {
+  getObservedFeatureValue,
+  getMaltreatmentAggregatedValue,
+  getMaltreatmentSelectedValues
+} from '../util';
+
+function DebugPlot({
+  mapType,
+  geography,
+  maltreatmentTypes,
+  observedFeature,
+  year,
+  selectedGeographicFeature,
+  data
+}) {
+  const getMaltreatmentTypeNames = maltreatmentTypeCodes => {
+    const updatedMaltreatmentTypesList = [];
+    if (maltreatmentTypeCodes.length === 0) {
+      return ['None'];
+    }
+    for (let i = 0; i < maltreatmentTypeCodes.length; i += 1) {
+      for (let j = 0; j < maltreatmentMeta.length; j += 1) {
+        if (maltreatmentTypeCodes[i] === maltreatmentMeta[j].field) {
+          updatedMaltreatmentTypesList.push(maltreatmentMeta[j].name);
+        }
+      }
+    }
+    return updatedMaltreatmentTypesList;
+  };
+
+  const getMaltreatmentTypesDataObject = (codeArray, nameArray, valueArray) => {
+    const newMaltreatmentDataObject = [];
+    for (let i = 0; i < codeArray.length; i += 1) {
+      const dataObject = {};
+      dataObject.code = codeArray[i];
+      dataObject.name = nameArray[i];
+      dataObject.value = valueArray[i];
+      newMaltreatmentDataObject.push(dataObject);
+    }
+    return newMaltreatmentDataObject;
+  };
+
+  const getSelectionDataList = (
+    mapTypeDebug,
+    geographyDebug,
+    yearDebug,
+    observedFeatureDebug,
+    observedGeographicFeatureDebug,
+    geoidDebug,
+    observedFeatureValueDebug,
+    maltreatmentTypesDataAggregateDebug
+  ) => {
+    return (
+      <ul>
+        <li>mapType: {mapTypeDebug}</li>
+        <li>geography: {geographyDebug}</li>
+        <li>observedFeature: {observedFeatureDebug}</li>
+        <li>observedFeatureValue: {observedFeatureValueDebug}</li>
+        <li>
+          observedGeographicFeature:
+          {observedGeographicFeatureDebug}
+        </li>
+        <li>geoid: {geoidDebug}</li>
+        <li>year: {yearDebug}</li>
+        <li>
+          maltreatmentTypesDataAggregate:
+          {maltreatmentTypesDataAggregateDebug}
+        </li>
+      </ul>
+    );
+  };
+
+  const getMaltreatmentDataTable = maltreatmentTypesDataObjectDebug => {
+    return (
+      <table className="debug-data-table">
+        <tr>
+          <th>type code</th>
+          <th>type name</th>
+          <th>type value</th>
+        </tr>
+        {maltreatmentTypesDataObjectDebug.map(maltreatmentTypeData => (
+          <tr>
+            <td>{maltreatmentTypeData.code}</td>
+            <td>{maltreatmentTypeData.name}</td>
+            <td>{maltreatmentTypeData.value}</td>
+          </tr>
+        ))}
+      </table>
+    );
+  };
+
+  const getDebugInfo = (selectionDataDebug, maltreatmentDataTableDebug) => {
+    return (
+      <div className="configurable-chart">
+        <div className="debug-info">
+          <div className="debug-status">DEBUGGING MODE ACTIVE</div>
+          <div className="debug-header">ConfigurableChart Component Data</div>
+          {selectionDataDebug}
+          {maltreatmentDataTableDebug}
+        </div>
+      </div>
+    );
+  };
+
+  const maltreatmentMeta = MALTREATMENT;
+  const geoid = selectedGeographicFeature;
+
+  const observedFeatureValue = getObservedFeatureValue(
+    data,
+    geography,
+    year,
+    geoid,
+    observedFeature
+  );
+
+  const maltreatmentTypesList = getMaltreatmentTypeNames(maltreatmentTypes);
+
+  const maltreatmentTypesDataValues = getMaltreatmentSelectedValues(
+    data,
+    geography,
+    year,
+    geoid,
+    maltreatmentTypes
+  );
+
+  const maltreatmentTypesDataAggregate = getMaltreatmentAggregatedValue(
+    data,
+    geography,
+    year,
+    geoid,
+    maltreatmentTypes
+  );
+
+  const maltreatmentTypesDataObject = getMaltreatmentTypesDataObject(
+    maltreatmentTypes,
+    maltreatmentTypesList,
+    maltreatmentTypesDataValues
+  );
+
+  const selectionDataList = getSelectionDataList(
+    mapType,
+    geography,
+    year,
+    observedFeature,
+    selectedGeographicFeature,
+    geoid,
+    observedFeatureValue,
+    maltreatmentTypesDataAggregate
+  );
+
+  const maltreatmentDataTable = getMaltreatmentDataTable(
+    maltreatmentTypesDataObject
+  );
+
+  const debugInfo = getDebugInfo(selectionDataList, maltreatmentDataTable);
+
+  return <div className="configurable-chart">{debugInfo}</div>;
+}
+
+DebugPlot.propTypes = {
+  mapType: PropTypes.string.isRequired,
+  geography: PropTypes.string.isRequired,
+  maltreatmentTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
+  observedFeature: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  selectedGeographicFeature: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.object.isRequired
+};
+
+export default DebugPlot;
---
+++ client/src/components/ProTx/components/charts/PredictiveFeaturesPlot.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Plot from 'react-plotly.js';
+import './PredictiveFeaturesPlot.css';
+import DebugPlot from './DebugPlot';
+import { OBSERVED_FEATURES, MALTREATMENT } from '../meta';
+import {
+  getObservedFeatureValue,
+  getMaltreatmentAggregatedValue,
+  getMaltreatmentSelectedValues
+} from '../util';
+
+// Set this to true to inspect the component data in a tabular view.
+// This will hide he chart rendering.
+const debugState = true;
+
+function PredictiveFeaturesPlot({
+  mapType,
+  geography,
+  maltreatmentTypes,
+  observedFeature,
+  year,
+  selectedGeographicFeature,
+  data
+}) {
+  if (debugState) {
+    // return <div className="configurable-chart">{debugInfo}</div>;
+    return (
+      <DebugPlot
+        className="plot-debug"
+        mapType={mapType}
+        geography={geography}
+        maltreatmentTypes={maltreatmentTypes}
+        observedFeature={observedFeature}
+        year={year}
+        selectedGeographicFeature={selectedGeographicFeature}
+        data={data}
+      />
+    );
+  }
+
+  return <div>PredictiveFeaturesPlot from ConfigurableChart refactor.</div>;
+}
+
+PredictiveFeaturesPlot.propTypes = {
+  mapType: PropTypes.string.isRequired,
+  geography: PropTypes.string.isRequired,
+  maltreatmentTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
+  observedFeature: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  selectedGeographicFeature: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.object.isRequired
+};
+
+export default PredictiveFeaturesPlot;
