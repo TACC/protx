7c2cc05ef4e28d5deeb4ea0d94c80cd04d7c67fa
John Gentle <taoteg@gmail.com>
2021-07-09 20:02:32 -0500

    Completed redesigning the ReportChart and the PredictiveFeaturesTable. Table now uses data instead of hard coded values. Content is in place to handle feature selection display when chart plot is completed. ReportCHart is currently commenting out Plots and Instructions until Plot is completed. This addresses tasks COOKS-78 and COOKS-80.

--- client/src/components/ProTx/components/charts/PredictiveFeaturesTable.js
+++ client/src/components/ProTx/components/charts/PredictiveFeaturesTable.js
@@ -1,94 +1,125 @@
 import React from 'react';
+import {
+  PREDICTIVE_FEATURES_TABLE_DATA,
+  PREDICTIVE_FEATURES_TABLE_NOTES
+} from './predictiveFeaturesTableData';
 import './PredictiveFeaturesTable.css';
 
-function PredictiveFeaturesTable() {
+const tableData = PREDICTIVE_FEATURES_TABLE_DATA;
+const tableNotes = PREDICTIVE_FEATURES_TABLE_NOTES;
+
+/**
+ * TODO: Pass in the selectedFeature object with its associated data.
+ * TODO: Use object for conditional rendering in the table.
+ */
+
+const selectedFeatureCheck = false;
+const selectedFeature = {
+  Demographic_Feature: 'SF_NAME',
+  Rank_By_Causal_Strength: 'SF_RBCS',
+  Rank_By_Random_Forest_Feature_Importance: 'SF_RBRFFI',
+  Average_Rank: 'SF_AR',
+  Ensemble_Rank: 'SF_ER'
+};
+
+const featuresTableHeaderRow = () => {
+  return (
+    <tr>
+      <th className="feature-table-chart-label">Demographic Feature</th>
+      <th className="feature-table-chart-cell ensemble-rank-label">
+        Ensemble Rank
+      </th>
+      <th className="feature-table-chart-cell">
+        Rank by Causal Strength <sup>a</sup>
+      </th>
+      <th className="feature-table-chart-cell">
+        Rank by Random Forest Feature Importance <sup>b</sup>
+      </th>
+      <th className="feature-table-chart-cell">Average Rank</th>
+    </tr>
+  );
+};
+
+const featureTableHeader = featuresTableHeaderRow();
+
+const featureTableData = tableData.map((data, index) => {
+  const i = index;
+  return (
+    <tr key={i}>
+      <td>{data.Demographic_Feature}</td>
+      <td className="ensemble-rank-value">{data.Ensemble_Rank}</td>
+      <td>{data.Rank_By_Causal_Strength}</td>
+      <td>{data.Rank_By_Random_Forest_Feature_Importance}</td>
+      <td>{data.Average_Rank}</td>
+    </tr>
+  );
+});
+
+const featureTableAnnotations = tableNotes.map((noteRow, index) => {
+  const i = index;
+  return (
+    <div className="feature-table-annotation" key={i}>
+      <span className="feature-table-annotation-prefix">
+        {noteRow.Note_Prefix}
+      </span>
+      <span className="feature-table-annotation-text">{noteRow.Note_Text}</span>
+    </div>
+  );
+});
+
+const getFeatureTable = () => {
+  if (selectedFeatureCheck) {
+    const getSelectedFeatureTableData = feature => {
+      const currentSelectedFeature = feature;
+      return (
+        <tr className="feature-table-selected-feature">
+          <td>{currentSelectedFeature.Demographic_Feature}</td>
+          <td className="ensemble-rank-value">
+            {currentSelectedFeature.Ensemble_Rank}
+          </td>
+          <td>{currentSelectedFeature.Rank_By_Causal_Strength}</td>
+          <td>
+            {currentSelectedFeature.Rank_By_Random_Forest_Feature_Importance}
+          </td>
+          <td>{currentSelectedFeature.Average_Rank}</td>
+        </tr>
+      );
+    };
+
+    const selectedFeatureTableData = getSelectedFeatureTableData(
+      selectedFeature
+    );
+
+    return (
+      <div className="feature-table">
+        <div className="feature-table-chart-selection">
+          <table>
+            {featureTableHeader}
+            {featureTableData}
+            {selectedFeatureTableData}
+          </table>
+        </div>
+        <div className="feature-table-info">{featureTableAnnotations}</div>
+      </div>
+    );
+  }
   return (
     <div className="feature-table">
       <div className="feature-table-chart">
         <table>
-          <tr>
-            <th className="feature-table-chart-label">Demographic Feature</th>
-            <th className="feature-table-chart-cell">
-              Rank by Causal Strength <sup>a</sup>
-            </th>
-            <th className="feature-table-chart-cell">
-              Rank by Random Forest Feature Importance <sup>b</sup>
-            </th>
-            <th className="feature-table-chart-cell">Average Rank</th>
-            <th className="feature-table-chart-cell">Ensemble Rank</th>
-          </tr>
-          <tr>
-            <td>Number of Single Parents</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>Number of Persons in Poverty</td>
-            <td>4</td>
-            <td>2</td>
-            <td>3</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>Per Capita Income</td>
-            <td>3</td>
-            <td>5</td>
-            <td>4</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>Number of Persons Age 17 and Younger</td>
-            <td>6</td>
-            <td>4</td>
-            <td>5</td>
-            <td>4</td>
-          </tr>
-          <tr>
-            <td>Number of Persons without High School Diploma</td>
-            <td>12</td>
-            <td>3</td>
-            <td>7.5</td>
-            <td>5</td>
-          </tr>
-          <tr>
-            <td>Number of Persons Living in Group Quarters</td>
-            <td>2</td>
-            <td>18</td>
-            <td>10</td>
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>Number of Households with Crowding</td>
-            <td>5</td>
-            <td>16</td>
-            <td>10.5</td>
-            <td>7</td>
-          </tr>
+          {featureTableHeader}
+          {featureTableData}
         </table>
       </div>
-
-      <div className="feature-table-info">
-        <div className="feature-table-description">
-          Table 1. Top seven features related to child maltreatment based on
-          county-level total maltreatment counts for the state of Texas,
-          2011-2019. Analysis was perfomed with two different models; features
-          are ranked according to their influences in each model type. The top
-          five features and rankings are shown for each model; features ranked
-          in the top five for both models are highlighted in yellow.
-        </div>
-        <div className="feature-table-annotation">
-          <sup>a</sup> Ranking by absolute value of estimated causal strength
-          where 1 indicates the largest causal impact.
-        </div>
-        <div className="feature-table-annotation">
-          <sup>b</sup> Ranking by random forest feature importance where 1
-          indicates the most important feature.
-        </div>
-      </div>
+      <div className="feature-table-info">{featureTableAnnotations}</div>
     </div>
   );
+};
+
+const featureTable = getFeatureTable();
+
+function PredictiveFeaturesTable() {
+  return featureTable;
 }
 
 export default PredictiveFeaturesTable;
