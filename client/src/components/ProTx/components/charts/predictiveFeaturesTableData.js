58bddaf1faf535fddf0bf5c6fca3bd63cf7b76de
John Gentle <taoteg@gmail.com>
2021-07-12 16:23:55 -0500

    Edited teh label text to use appropriate capitalization.

--- client/src/components/ProTx/components/charts/predictiveFeaturesTableData.js
+++ client/src/components/ProTx/components/charts/predictiveFeaturesTableData.js
@@ -1,48 +1,48 @@
 export const PREDICTIVE_FEATURES_TABLE_DATA = [
   {
-    Demographic_Feature: 'number of single parents',
+    Demographic_Feature: 'Number of Single Parents',
     Rank_By_Causal_Strength: '1',
     Rank_By_Random_Forest_Feature_Importance: '1',
     Average_Rank: '1.0',
     Ensemble_Rank: '1'
   },
   {
-    Demographic_Feature: 'number of persons in poverty',
+    Demographic_Feature: 'Number of Persons in Poverty',
     Rank_By_Causal_Strength: '4',
     Rank_By_Random_Forest_Feature_Importance: '2',
     Average_Rank: '3.0',
     Ensemble_Rank: '2'
   },
   {
-    Demographic_Feature: 'per capita income',
+    Demographic_Feature: 'Per Capita Income',
     Rank_By_Causal_Strength: '3',
     Rank_By_Random_Forest_Feature_Importance: '5',
     Average_Rank: '4.0',
     Ensemble_Rank: '3'
   },
   {
-    Demographic_Feature: 'number of persons age 17 and younger',
+    Demographic_Feature: 'Number of Persons Age 17 and Younger',
     Rank_By_Causal_Strength: '6',
     Rank_By_Random_Forest_Feature_Importance: '4',
     Average_Rank: '5.0',
     Ensemble_Rank: '4'
   },
   {
-    Demographic_Feature: 'number of persons without high school diploma',
+    Demographic_Feature: 'Number of Persons without High School Diploma',
     Rank_By_Causal_Strength: '12',
     Rank_By_Random_Forest_Feature_Importance: '3',
     Average_Rank: '7.5',
     Ensemble_Rank: '5'
   },
   {
-    Demographic_Feature: 'number of persons living in group quarters',
+    Demographic_Feature: 'Number of Persons Living in Group Quarters',
     Rank_By_Causal_Strength: '2',
     Rank_By_Random_Forest_Feature_Importance: '18',
     Average_Rank: '10.0',
     Ensemble_Rank: '6'
   },
   {
-    Demographic_Feature: 'number of households with crowding',
+    Demographic_Feature: 'Number of Households with Crowding',
     Rank_By_Causal_Strength: '5',
     Rank_By_Random_Forest_Feature_Importance: '16',
     Average_Rank: '10.5',
