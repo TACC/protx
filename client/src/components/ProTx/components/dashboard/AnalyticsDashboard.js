866c0d64fe7b161c7273afce805ea15ea9b7e7ec
John Gentle <taoteg@gmail.com>
2021-08-24 11:47:14 -0500

    Updated AnalyticsDashboard.js hostName in header msg.
    Tweak to the header message text.

--- client/src/components/ProTx/components/dashboard/AnalyticsDashboard.js
+++ client/src/components/ProTx/components/dashboard/AnalyticsDashboard.js
@@ -7,7 +7,7 @@ import './AnalyticsDashboard.css';
 // Need to find a way to set this value based on some external configuration value so it changes based on the hosts settings.
 // false for PROD, true for all non-PROD deployments.
 const showHeader = true;
-const hostName = 'COOKS PRE-PRODUCTION PORTAL';
+const hostName = 'COOK CHILDREN\'S PRE-PRODUCTION PORTAL';
 const messageString =
   'Geospatial data analytics dashboard using incomplete demonstration data.';
 
