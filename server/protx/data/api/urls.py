98675b374985de09855f97b96bd8926dee93f1b0
Nathan Franklin <nathan.franklin@gmail.com>
2021-10-06 10:18:04 -0500

    Add demographics query/route using database

--- server/protx/data/api/urls.py
+++ server/protx/data/api/urls.py
@@ -4,4 +4,5 @@ from protx.data.api import views
 app_name = 'protx_api'
 urlpatterns = [
     url('maltreatment/', views.get_maltreatment, name='data'),
+    url('demographics/', views.get_demographics, name='data')
 ]
