aa89fd3cf94484626bdd15ba22ce693d1281149e
John Gentle <taoteg@gmail.com>
2021-10-07 15:48:31 -0500

    Basic implementation of subplotted rgoupings of nine traces on mock data.

--- client/src/components/ProTx/components/shared/plotUtils.js
+++ client/src/components/ProTx/components/shared/plotUtils.js
@@ -306,7 +306,7 @@ const getObservedFeaturesPlotData = () => {
   const newObservedFeaturesPlotData = getObservedFeaturesDataObject();
 
   // Transform the backend response from the query into the required object structure for the plot.
-  const y = newObservedFeaturesPlotData.fig_aes.bar_labels;
+  const x = newObservedFeaturesPlotData.fig_aes.bar_labels;
   // Need to iterate instead of declare every year...
   const y0 = newObservedFeaturesPlotData.fig_aes.years.[2011].bars;
   const y1 = newObservedFeaturesPlotData.fig_aes.years.[2012].bars;
@@ -318,108 +318,194 @@ const getObservedFeaturesPlotData = () => {
   const y7 = newObservedFeaturesPlotData.fig_aes.years.[2018].bars;
   const y8 = newObservedFeaturesPlotData.fig_aes.years.[2019].bars;
 
-  const trace0 = {
-    y: y0,
-    type: 'histogram',
-    opacity: 0.5,
-    marker: {
-      color: histColors[0]
-    }
-  };
 
+  // const trace0 = {
+  //   x: x,
+  //   y: y0,
+  //   type: 'histogram',
+  //   opacity: 0.5,
+  //   marker: {
+  //     color: histColors[0]
+  //   }
+  // };
+
+  // const trace1 = {
+  //   x: x,
+  //   y: y1,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[1]
+  //   }
+  // };
+
+  // const trace2 = {
+  //   x: x,
+  //   y: y2,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[2]
+  //   }
+  // };
+
+  // const trace3 = {
+  //   x: x,
+  //   y: y3,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[3]
+  //   }
+  // };
+
+  // const trace4 = {
+  //   x: x,
+  //   y: y4,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[4]
+  //   }
+  // };
+
+  // const trace5 = {
+  //   x: x,
+  //   y: y5,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[5]
+  //   }
+  // };
+
+  // const trace6 = {
+  //   x: x,
+  //   y: y6,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[6]
+  //   }
+  // };
+
+  // const trace7 = {
+  //   x: x,
+  //   y: y7,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color:histColors[7]
+  //   }
+  // };
+
+  // const trace8 = {
+  //   x: x,
+  //   y: y8,
+  //   type: 'histogram',
+  //   opacity: 0.6,
+  //   marker: {
+  //     color: histColors[8]
+  //   }
+  // };
+
+  // DUMMY DATA.
   const trace1 = {
-    y: y1,
-    type: 'histogram',
-    opacity: 0.6,
-    marker: {
-      color: histColors[1]
-    }
+    x: [1, 2, 3],
+    y: [4, 5, 6],
+    xaxis: 'x1',
+    yaxis: 'y1',
+    type: 'scatter'
   };
 
   const trace2 = {
-  y: y2,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[2]
-  }
-};
+    x: [20, 30, 40],
+    y: [50, 60, 70],
+    xaxis: 'x2',
+    yaxis: 'y2',
+    type: 'scatter'
+  };
 
   const trace3 = {
-  y: y3,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[3]
-  }
-};
+    x: [300, 400, 500],
+    y: [600, 700, 800],
+    xaxis: 'x3',
+    yaxis: 'y3',
+    type: 'scatter'
+  };
 
   const trace4 = {
-  y: y4,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[4]
-  }
-};
+    x: [20, 30, 40],
+    y: [50, 60, 70],
+    xaxis: 'x4',
+    yaxis: 'y4',
+    type: 'scatter'
+  };
 
   const trace5 = {
-  y: y5,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[5]
-  }
-};
+    x: [1, 2, 3],
+    y: [4, 5, 6],
+    xaxis: 'x5',
+    yaxis: 'y5',
+    type: 'scatter'
+  };
 
   const trace6 = {
-  y: y6,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[6]
-  }
-};
+    x: [20, 30, 40],
+    y: [50, 60, 70],
+    xaxis: 'x6',
+    yaxis: 'y6',
+    type: 'scatter'
+  };
 
   const trace7 = {
-  y: y7,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color:histColors[7]
-  }
-};
+    x: [300, 400, 500],
+    y: [600, 700, 800],
+    xaxis: 'x7',
+    yaxis: 'y7',
+    type: 'scatter'
+  };
 
   const trace8 = {
-  y: y8,
-  type: 'histogram',
-  opacity: 0.6,
-  marker: {
-    color: histColors[8]
-  }
-};
+    x: [20, 30, 40],
+    y: [50, 60, 70],
+    xaxis: 'x8',
+    yaxis: 'y8',
+    type: 'scatter'
+  };
+
+  const trace9 = {
+    x: [0, 15, 30],
+    y: [7, 9, 11],
+    xaxis: 'x9',
+    yaxis: 'y9',
+    type: 'scatter'
+  };
+
 
   const observedFeaturesDataObject = [
-  trace0,
-  // trace1,
-  // trace2,
-  // trace3,
-  // trace4,
-  // trace5,
-  // trace6,
-  // trace7,
-  // trace8
-];
+    trace1,
+    trace2,
+    trace3,
+    trace4,
+    trace5,
+    trace6,
+    trace7,
+    trace8,
+    trace9
+  ];
 
   const plotTitle = 'Demographics';
-  const plotOrientation = 'h';
+  const plotOrientation = 'v';
   const showPlotLegend = false;
   const plotXDataLabel = 'X DATA LABEL';
   const plotXDataAxisType = 'linear';
   const plotYDataLabel = 'Y DATA LABEL';
   const plotYDataAxisType = 'category';
+  const plotSubplotGrids = { grid: { rows: 3, columns: 3, pattern: 'independent' } };
+  // const plotSubplotGrids = { grid: { rows: 1, columns: 9, pattern: 'independent' }};
 
-  const plotLayout = getPlotLayout(
+  const basePlotLayout = getPlotLayout(
     plotTitle,
     plotOrientation,
     showPlotLegend,
@@ -428,6 +514,11 @@ const getObservedFeaturesPlotData = () => {
     plotYDataLabel,
     plotYDataAxisType
   );
+
+  const plotLayout = {
+    ...basePlotLayout,
+    ...plotSubplotGrids
+  };
 
   // const plotData = getPlotDataBars(
   //   'observed',
