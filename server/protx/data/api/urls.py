aacaea536efc085b152b345b5f18a0bf1352d71a
Nathan Franklin <nathan.franklin@gmail.com>
2021-10-08 06:45:19 -0500

    Add route for display data information

--- server/protx/data/api/urls.py
+++ server/protx/data/api/urls.py
@@ -3,6 +3,7 @@ from protx.data.api import views
 
 app_name = 'protx_api'
 urlpatterns = [
+    url('display/', views.get_display, name='data'),
     url('maltreatment/', views.get_maltreatment, name='data'),
     url('demographics/', views.get_demographics, name='data')
 ]
