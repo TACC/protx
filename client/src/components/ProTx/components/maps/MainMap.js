6547ed013578bbbdca8d1b2867ebfc8bd5124207
John Gentle <taoteg@gmail.com>
2021-11-23 16:19:13 -0600

    Basic popup content is now inplace. Need to add styling for layout.

--- client/src/components/ProTx/components/maps/MainMap.js
+++ client/src/components/ProTx/components/maps/MainMap.js
@@ -174,7 +174,55 @@ function MainMap({
         const marker = L.marker(L.latLng(point.LATITUDE, point.LONGITUDE), {
           title: point.NAME
         });
-        marker.bindPopup(point.NAME);
+        // marker.bindPopup(point.NAME);
+
+        const popupValues = {
+          name: '',
+          description: '',
+          street: '',
+          city: '',
+          state: '',
+          postal_code: '',
+          phone: '',
+          website: ''
+        };
+
+        if (point.NAME !== null) {
+          popupValues.name = point.NAME;
+        }
+        if (point.HOVER_DESCRIPTION !== null) {
+          popupValues.description = point.HOVER_DESCRIPTION;
+        }
+        if (point.STREET !== null) {
+          popupValues.street = point.STREET;
+        }
+        if (point.CITY !== null) {
+          popupValues.city = point.CITY;
+        }
+        if (point.STATE !== null) {
+          popupValues.state = point.STATE;
+        }
+        if (point.POSTAL_CODE !== null) {
+          popupValues.postal_code = point.POSTAL_CODE;
+        }
+        if (point.PHONE !== null) {
+          popupValues.phone = point.PHONE;
+        }
+        if (point.WEBSITE !== null) {
+          popupValues.website = point.WEBSITE;
+        }
+
+        const popupContent = `<div className="marker-popup-content">
+            <div>${popupValues.name}</div>
+            <div>${popupValues.description}</div>
+            <div>${popupValues.street}</div>
+            <div>${popupValues.city}</div>
+            <div>${popupValues.state}</div>
+            <div>${popupValues.postal_code}</div>
+            <div>${popupValues.phone}</div>
+            <div>${popupValues.website}</div>
+          </div>`;
+        marker.bindPopup(popupContent);
         resourcesClusterGroups[point.NAICS_CODE].addLayers(marker);
       });
 
