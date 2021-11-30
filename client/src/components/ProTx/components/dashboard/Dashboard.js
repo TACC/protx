7686b197a430ace22a078addfd2e1dc1e8c8b14d
Nathan Franklin <nathan.franklin@gmail.com>
2021-11-29 21:24:39 -0600

    Redirect to onboarding if user tries to access protx

--- client/src/components/ProTx/components/dashboard/Dashboard.js
+++ client/src/components/ProTx/components/dashboard/Dashboard.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useRouteMatch, Redirect } from 'react-router-dom';
 import DashboardHeader from './DashboardHeader';
 import DashboardDisplay from './DashboardDisplay';
 import '../shared/Variables.css';
 import './Dashboard.css';
+import * as ROUTES from '../../../../constants/routes';
 
 /**
  * TODO: find a way to set this value based on some external configuration value so it changes based on the hosts settings.
@@ -15,6 +18,9 @@ const messageString =
   'Geospatial data analytics dashboard using demonstration data.';
 
 function Dashboard() {
+  const { path } = useRouteMatch();
+  const setupComplete = useSelector(state => state.workbench.setupComplete);
+
   if (showHeader) {
     return (
       <div className="dashboard-root-header">
@@ -26,7 +32,14 @@ function Dashboard() {
 
   return (
     <div className="dashboard-root">
-      <DashboardDisplay />
+      {setupComplete ? (
+        <DashboardDisplay />
+      ) : (
+        <Redirect
+          from={`${path}`}
+          to={`${ROUTES.WORKBENCH}${ROUTES.ONBOARDING}/setup/`}
+        />
+      )}
     </div>
   );
 }
