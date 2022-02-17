0e070e028cb58da03b2070784ffdddfe02866855
John Gentle <taoteg@gmail.com>
2022-02-17 14:15:56 -0600

    Sorted the urls routes alphabetically. Added in placeholders for maltreatment and analtics server-side plotting logic.

--- server/protx/data/api/urls.py
+++ server/protx/data/api/urls.py
@@ -4,10 +4,10 @@ from protx.data.api import views
 
 app_name = 'protx_api'
 urlpatterns = [
+    url('demographics/', views.get_demographics, name='data'),
+    path('demographics-plot-distribution/<area>/<geoid>/<variable>/<unit>/', views.get_demographics_distribution_plot_data, name='data'),
     url('display/', views.get_display, name='data'),
     url('maltreatment/', views.get_maltreatment, name='data'),
-    url('demographics/', views.get_demographics, name='data'),
-    url('resources/', views.get_resources, name='data'),
-    path('demographics-plot-distribution/<area>/<geoid>/<variable>/<unit>/', views.get_demographics_distribution_plot_data, name='data'),
-    path('maltreatment-plot-distribution/<area>/<geoid>/<variable>/<unit>/', views.get_maltreatment_distribution_plot_data, name='data')
+    path('maltreatment-plot-distribution/<area>/<geoid>/<variable>/<unit>/', views.get_maltreatment_distribution_plot_data, name='data'),
+    url('resources/', views.get_resources, name='data')
 ]
